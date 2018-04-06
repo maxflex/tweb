af233f1f8a6312c83853499f8b73b74bf91ad5af
maxflex <makcyxa-k@yandex.ru>
2018-04-06 16:08:42 +0300

    Equipment update

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -10,11 +10,11 @@ class Equipment extends Service\Model
 
     public function getContrastAttribute()
     {
-        list($r, $g, $b) = sscanf($this->color, "#%02x%02x%02x");
-        if (($r * 0.299 + $g * 0.587 + $b * 0.114) > 186) {
-            return 'black';
-        } else {
+        list($r, $g, $b) = sscanf($this->color, "%02x%02x%02x");
+        if ($r * 0.299 + $g * 0.587 + $b * 0.114 > 186) {
             return 'white';
+        } else {
+            return 'black';
         }
     }
 }
