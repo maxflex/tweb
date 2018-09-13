19b4c36650cfb662f033fea3e2cad7995c465b68
maxflex <makcyxa-k@yandex.ru>
2018-09-13 17:52:16 +0300

    Print div

--- resources/assets/js/engine.js
+++ resources/assets/js/engine.js
@@ -203,3 +203,35 @@ function fileChange(event) {
         $('.uploaded-photo-box').last().css('background-image', "url('" + URL.createObjectURL(event.target.files[0]) + "')")
     }, 100)
 }
+
+/**
+ * Печать дива.
+ *
+ */
+function printDiv(id_div) {
+    var contents = document.getElementById(id_div).innerHTML;
+    var frame1 = document.createElement('iframe');
+    frame1.name = "frame1";
+    frame1.style.position = "absolute";
+    frame1.style.top = "-1000000px";
+
+    document.body.appendChild(frame1);
+    var frameDoc = frame1.contentWindow ? frame1.contentWindow : frame1.contentDocument.document ? frame1.contentDocument.document : frame1.contentDocument;
+    frameDoc.document.open();
+    frameDoc.document.write('<html><head><title>Ателье «Талисман»</title>');
+    frameDoc.document.write("<style type='text/css'>\
+    	h4 {text-align: center}\
+    	p {text-indent: 50px; margin: 0}\
+	  </style>"
+	); 
+    frameDoc.document.write('</head><body>');
+    frameDoc.document.write(contents);
+    frameDoc.document.write('</body></html>');
+    frameDoc.document.close();
+    setTimeout(function () {
+        window.frames["frame1"].focus();
+        window.frames["frame1"].print();
+        document.body.removeChild(frame1);
+    }, 500);
+    return false;
+}
