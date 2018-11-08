d23d91bd2524e0b46f17fa6f9d352b652c9cbfb2
maxflex <>
2018-11-08 14:43:17 +0300

    Order email

--- resources/views/mail/order.blade.php
+++ resources/views/mail/order.blade.php
@@ -9,11 +9,10 @@
 </div>
 @if (isset($request->photos) && count($request->photos))
     <div>
-        Фотографии:
         <ul>
             @foreach ($request->photos as $index => $url)
                 <li>
-                    <a href='{{ config('app.url') }}{{ \App\Models\Photo::UPLOAD_DIR }}{{ $url }}'>{{ $index + 1 }}</a>
+                    <a href='{{ config('app.url') }}{{ \App\Models\Photo::UPLOAD_DIR }}{{ $url }}'>Фотография {{ $index + 1 }}</a>
                 </li>
             @endforeach
         </ul>
