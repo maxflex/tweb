87841c98103ed01b55fae3d2b7f04688bc2b30d9
maxflex <>
2018-12-11 10:55:45 +0300

    symbol limits removed

--- app/Http/Requests/RequestStore.php
+++ app/Http/Requests/RequestStore.php
@@ -25,14 +25,14 @@ class RequestStore extends FormRequest
     {
         return [
             'phone'   => ['required', 'phone_filled', 'starts_with_correct_digit'],
-            'name'    => [
-                'regex:' . TEXT_VALIDATION_REGEX,
-                'max:' . MAX_NAME_LENGTH,
-            ],
-            'comment' => [
-                'regex:' . TEXT_VALIDATION_REGEX,
-                'max:' . MAX_COMMENT_LENGTH,
-            ]
+            // 'name'    => [
+            //     'regex:' . TEXT_VALIDATION_REGEX,
+            //     'max:' . MAX_NAME_LENGTH,
+            // ],
+            // 'comment' => [
+            //     'regex:' . TEXT_VALIDATION_REGEX,
+            //     'max:' . MAX_COMMENT_LENGTH,
+            // ]
         ];
     }
 
