ba0af658210a2c3bb6749873f9b601824c830932
root <makcyxa-k@yandex.ru>
2019-01-14 23:53:47 +0300

    remote changes

--- app/Http/Controllers/Api/RequestsController.php
+++ app/Http/Controllers/Api/RequestsController.php
@@ -19,5 +19,15 @@ class RequestsController extends Controller
             'data' => json_encode($request->all())
         ]);
         Mail::send(new Order($request->all()));
+
+        
+        $roistat = $_COOKIE['roistat_visit'];
+        $formName = 'Расчет ремонта';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'http://bik.agency/mycrm/add.php?roistatID='.urlencode($roistat).'&formName='.urlencode($formName).'&domain=http://souz-pribor.ru'.'&formData[ФИО]='.urlencode($request->name).'&formData[Телефон]='.urlencode($request->phone));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $out = curl_exec($curl);
+        curl_close($curl);
+
     }
 }
