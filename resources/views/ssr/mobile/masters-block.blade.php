02280236fe72a74a787cdeadd7bab5cb495c2a22
maxflex <>
2019-03-31 17:56:40 +0300

    style updates

--- resources/views/ssr/mobile/masters-block.blade.php
+++ resources/views/ssr/mobile/masters-block.blade.php
@@ -1,4 +1,4 @@
-<div class="full-width gray-full-width" style='margin-bottom: 60px'>
+<div class="full-width gray-full-width" @if (! $all)style='margin-bottom: 60px'@endif>
     <div class="common" style='display: inline-block'>
         <div class='header-1' style='padding-bottom: 30px'>{{ $title }}</div>
         <div class="main-page-masters">
@@ -22,10 +22,24 @@
             </div>
             @endforeach
         </div>
-         <center class='more-button' style='margin: 40px 0 0'>
+        @if (! $all)
+        <center class='more-button' style='margin: 40px 0 0'>
             <a class='no-style-link' href='/masters/'>
                 <button class="btn-border">смотреть всех</button>
             </a>
         </center>
+        @endif
     </div>
 </div>
+@if ($all)
+<style>
+    footer {
+        margin-top: 0 !important;
+    }
+
+    .gray-full-width {
+        padding-bottom: 0;
+        margin-bottom: 0;
+    }
+</style>
+@endif
