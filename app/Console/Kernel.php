156e9d6ee6be9df45172e41779452c394420b11e
maxflex <>
2019-05-23 00:24:56 +0300

    Convert images

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\FindAllLinks::class,
         Commands\FindAllLinksNew::class,
+        Commands\ConvertImages::class,
     ];
 
     /**
