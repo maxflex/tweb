e6ee2410c3c4989762d18135e3419d5b91e47114
maxflex <>
2019-05-31 01:23:49 +0300

    directive image update

--- resources/views/directives/gallery-item-main.blade.php
+++ resources/views/directives/gallery-item-main.blade.php
@@ -1,5 +1,5 @@
 <div ng-click="service.next()">
-    <img ng-src='@{{ item.url }}' class="gallery-photo pointer">
+    <img ng-src='@{{ item.thumb }}' class="gallery-photo pointer">
 </div>
 <div class="gallery-header">
     @{{ item.name }}
