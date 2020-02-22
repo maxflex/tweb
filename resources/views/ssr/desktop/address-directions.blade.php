6b43b3721ff88164f6493703921e6dcce97ed0ac
maxflex <makcyxa-k@yandex.ru>
2020-02-22 15:20:32 +0300

    Address Directions

--- resources/views/ssr/desktop/address-directions.blade.php
+++ resources/views/ssr/desktop/address-directions.blade.php
@@ -1,8 +1,8 @@
-<div class="service-list" style="margin-top: 0">
+<div class="service-list">
     {{-- FIRST ITEM --}}
     @if ($firstItem)
         <div class="service-list__items service-list__items_one-line">
-            <div style='width: 100%; display: flex; align-items: center;'>
+            <div style='width: 100%; display: flex; align-items: center; margin-top: 0'>
                 <div>
                     @if ($firstItem->title)
                         <a class='no-style-link' href='/{{ $firstItem->link_url }}/'>
