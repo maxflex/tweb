cc4d0e03904291a05d70cdd9e589b34ad00bb461
maxflex <makcyxa-k@yandex.ru>
2020-03-17 23:27:01 +0300

    Video order

--- app/Service/Ssr/Variables/VideoBlock.php
+++ app/Service/Ssr/Variables/VideoBlock.php
@@ -11,9 +11,11 @@ class VideoBlock extends SsrVariable
     public function parse()
     {
         $ids = array_filter(explode(',', $this->args->ids));
-        $query = Video::whereIn('id', $ids)->orderByPosition();
+        $query = Video::whereIn('id', $ids);
         if (count($ids)) {
             $query->orderBy(DB::raw('FIELD(id, ' . implode(',', $ids) . ')'));
+        } else {
+            $query->orderByPosition();
         }
         $items = $query->get();
         return view($this->getViewName(), [
