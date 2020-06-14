361038d7486c9118a032c8db7443835dc30e2887
maxflex <makcyxa-k@yandex.ru>
2020-06-14 18:54:40 +0300

    New icons 2

--- resources/views/ssr/desktop/address-info.blade.php
+++ resources/views/ssr/desktop/address-info.blade.php
@@ -7,19 +7,19 @@
     <div class="flex-items address-info-block">
         <div>
             <div class="flex-items">
-                <img src="/img/icons/border/marker.png" />
+                <img src="/img/icons/color/marker.png" />
                 <span>
                     {{ $info->address }}
                 </span>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/clock.png" />
+                <img src="/img/icons/color/clock.png" />
                 <span>
                     09:00-21:00 без выходных
                 </span>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/route.png" />
+                <img src="/img/icons/color/route.png" />
                 <a href="{{ $info->route }}" target="_blank">
                 Проложить маршрут
                 </a>
@@ -27,7 +27,7 @@
         </div>
         <div>
             <div class="flex-items">
-                <img src="/img/icons/border/phone.png" />
+                <img src="/img/icons/color/phone.png" />
                 <div class="flex-items">
                     <a href="tel:84952152231">
                         +7 495 215-22-31
@@ -39,13 +39,13 @@
             </div>
 
             <div class="flex-items">
-                <img src="/img/icons/border/whatsapp.png" />
+                <img src="/img/icons/color/whatsapp.png" />
                 <a href="whatsapp://send?phone=+79057464481">
                     +7 903 763 15 21
                 </a>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/viber.png" />
+                <img src="/img/icons/color/viber.png" />
                 <a href="viber://chat?number=+79057464481">
                     +7 903 763 15 21
                 </a>
@@ -53,19 +53,19 @@
         </div>
         <div>
             <div class="flex-items">
-                <img src="/img/icons/border/globe.png" />
+                <img src="/img/icons/color/globe.png" />
                 <a href="https://atelier-talisman.ru">
                     atelier-talisman.ru
                 </a>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/mail.png" />
+                <img src="/img/icons/color/mail.png" />
                 <a href="mail:info@atelier-talisman.ru">
                     info@atelier-talisman.ru
                 </a>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/card.png" />
+                <img src="/img/icons/color/card.png" />
                 <span>
                     Оплата по картам
                 </span>
@@ -73,19 +73,19 @@
         </div>
         <div>
             <div class="flex-items">
-                <img src="/img/icons/border/instagram.png" />
+                <img src="/img/icons/color/instagram.png" />
                 <a href="https://instagram.com/atelier_talisman" target="_blank">
                     Instagram
                 </a>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/vk.png" />
+                <img src="/img/icons/color/vk.png" />
                 <a href="https://vk.com/atelier_talisman" target="_blank">
                     ВК
                 </a>
             </div>
             <div class="flex-items">
-                <img src="/img/icons/border/youtube.png" />
+                <img src="/img/icons/color/youtube.png" />
                 <a href="https://www.youtube.com/channel/UCCIMu941ZNGNMhDwlaWHNSA" target="_blank">
                     Youtube
                 </a>
