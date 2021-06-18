61da9dfde585edb598490994c7afee9e20c3f27a
root <makcyxa-k@yandex.ru>
2021-06-18 13:25:56 +0300

    Add from server

--- app/Console/Commands/FindAllLinks.php
+++ app/Console/Commands/FindAllLinks.php
@@ -3,9 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\Page;
-use App\Models\Variable;
-use Storage;
 
 class FindAllLinks extends Command
 {
@@ -40,83 +37,32 @@ class FindAllLinks extends Command
      */
     public function handle()
     {
-        $links = [];
-
-        foreach(Page::all() as $page) {
-            $links = array_merge($links, self::findHrefs($page->getClean('html'), $page->url));
-            $links = array_merge($links, self::findHrefs($page->getClean('html_mobile'), $page->url));
-        }
-
-        foreach(Variable::all() as $variable) {
-            $links = array_merge($links, self::findHrefs($variable->getClean('html')));
-        }
-
-        foreach($links as &$link) {
-            if ($link[0] == '/' || strpos($link, 'http') !== false) {
-                if ($link[strlen($link) - 1] != '/' && strpos($link, '.pdf') === false && strpos($link, '.css') === false && strpos($link, '.js') === false && strpos($link, '.png') === false) {
-                    $link = $link . '/';
-                }
-            }
-        }
-
-        $links = array_unique($links);
-        sort($links);
-
-        Storage::put('links.txt', implode("\n", $links));
-
-        $custom_check = ["", "================", "CUSTOM CHECK", "================"];
-        $error_links  = ["================", "ERROR LINKS", "================"];
-
-        foreach($links as $link) {
-            if ($link[0] == '/' && strpos($link, 'http') === false) {
-                $status_code = self::getStatusCode($link);
-                dump($link . " | " . $status_code);
-                if ($status_code != 200) {
-                    $this->error("error");
-                    $error_links[] = $link;
-                } else {
-                    $this->info("ok");
-                }
+        $orders = \DB::table('request_log')->where('id', '>=', 1885)->get();
+        $result = [['дата', 'имя', 'телефон', 'комментарий', 'фото']];
+        foreach($orders as $order) {
+            $data = json_decode($order->data);
+            if (isset($data->phone)) {
+                // dump($data);
+                $result[] = [
+                    $order->created_at,
+                    @$data->name,
+                    \App\Service\Phone::format('7' . $data->phone),
+                    @$data->comment,
+                    implode("\n", array_map(function ($url) {
+                        return config('app.url') . 'request-photos/' . $url;
+                    }, $data->photos))
+                ];
             } else {
-                $this->line("custom: $link");
-                $custom_check[] = $link;
+                // dump($order);
+                $order->data = $order->data . '"}';
+                $data = json_decode($order->data);
+                dump($order);
             }
         }
-
-        Storage::put('link_problems.txt', implode("\n", array_merge($error_links, $custom_check)));
-    }
-
-    /**
-     * $prefix – если ссылка не от корня, учитываем URL страницы
-     */
-    private static function findHrefs($html, $prefix = null)
-    {
-        preg_match_all('/href=\'(.+)\'/U', $html, $m1);
-        preg_match_all('/href="(.+)"/U', $html, $m2);
-        $links = array_merge($m1[1], $m2[1]);
-        if ($prefix) {
-            if ($prefix[strlen($prefix) - 1] != '/') {
-                $prefix = $prefix . '/';
-            }
-            if ($prefix[0] != '/') {
-                $prefix = '/' . $prefix;
-            }
-            foreach($links as &$link) {
-                if ($link[0] != '/' && strpos($link, 'http') === false) {
-                    $link = $prefix . $link;
-                }
-            }
-        }
-
-        return $links;
-    }
-
-    private static function getStatusCode($url)
-    {
-        $handle = curl_init('https://ege-centr.ru' . $url);
-        curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
-        $response = curl_exec($handle);
-        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-        return $httpCode;
+        // $fp = fopen('orders.csv', 'w');
+        // foreach ($result as $fields) {
+        //     fputcsv($fp, $fields);
+        // }
+        // fclose($fp);
     }
 }
